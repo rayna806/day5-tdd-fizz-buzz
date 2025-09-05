f642d4239f1cc462280563ce9cc8e2a02271f565
rayna806 <LUORA2@OOCL.COM>
2025-09-05 10:55:45 +0800

    Test case 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_input_1(){
+        int order = 1;
+         String word = FizzBuzz.countOff(order);
+         assertEquals("1",word);
+
+    }
 }
